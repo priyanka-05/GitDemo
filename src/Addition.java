412fbe35aec3444d724dd3f0912a08279fa186e2
Priyanka <Priyanka@LAPTOP-G1Q7HEVR>
2020-02-04 15:26:39 +0530

    first commit

---
+++ src/Addition.java
@@ -0,0 +1,17 @@
+
+public class Addition {
+	
+	public static void main(String[] args) 
+	
+	{
+		int a=10;
+		int b=20;
+		int c=30;
+		int d=a+b+c;
+		
+	System.out.println("enter addition of three numbers" +d);
+	
+		
+	}
+
+}
