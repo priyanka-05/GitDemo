47a264cecc5345af6477f84ad1149abc9e90893b
priyanka-05 <60646587+priyanka-05@users.noreply.github.com>
2020-02-04 16:03:03 +0530

    Update Multiplication.java

--- src/Multiplication.java
+++ src/Multiplication.java
@@ -2,7 +2,10 @@
 public class Multiplication{
 	public static void main(String[] args) {
 		System.out.println("Inside Eclipse");
+		System.out.println("Inside Master");
 		System.out.println("Inside Master1");
+		
+		
 	}
 	}
 	
